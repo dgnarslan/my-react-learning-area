01bc195d11953c5189ba40a2f8323ad0049f5d03
dgnarslan <arslan.dgn51@gmail.com>
2023-12-07 23:08:10 +0300

    First lesson finished

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import routes from './routes/index.jsx';
+import { RouterProvider } from 'react-router-dom';
+import '~/assets/css/tailwind.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={routes} />,
+);
