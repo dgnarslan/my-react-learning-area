6ace9397b9bb69aa8e403ab1031dfe87c800dad8
dgnarslan <arslan.dgn51@gmail.com>
2023-12-14 23:11:28 +0300

    Ninth lesson finished

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,3 +1,19 @@
+import StickyHeader from '~/components/sticky-header/index.jsx';
+import Tab from '~/components/tab/index.jsx';
+
 export default function Home() {
-  return <div>Home component</div>;
+  return (
+    <>
+      <Tab activeTab={'followings'}>
+        <StickyHeader title={'Anasayfa'}>
+          <Tab.Items>
+            <Tab.Item id={'for-you'}>Sana Özel</Tab.Item>
+            <Tab.Item id={'followings'}>Takip edilenler</Tab.Item>
+          </Tab.Items>
+        </StickyHeader>
+        <Tab.Content id={'for-you'}>1.content</Tab.Content>
+        <Tab.Content id={'followings'}>2.content</Tab.Content>
+      </Tab>
+    </>
+  );
 }
